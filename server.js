aa77c4ebfe54ee7ec831ec4aeb3c53f0ec3e776f
anmolkys <feimoxin@outlook.com>
2023-10-30 12:38:13 +0530

    added send file

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express();
 const http = require("http").createServer(app);
 
 const jsonData = require("./csvjson.json");
-console.log(jsonData.length);
 
 app.get("/random",(req,res)=>{
     let index = Math.floor(Math.random()*(jsonData.length+1));
@@ -19,11 +18,14 @@ app.get("/:id",(req,res)=>{
     let list = [];
     for(let i = 0; i<id ; i++){
         let index = randomImage();
-        list = [...list,{id:index,name: jsonData[index].Character,url:__dirname +`/${jsonData[index].Image_URL}`}]
+        list = [...list,{id:index,name: jsonData[index].Character, url:`https://waifuserv.onrender.com/waifu/${index}`}]
     }
     res.send(list);
 })
-
+app.get("/waifu/:index",(req,res)=>{
+    let index = req.params.index;
+    res.sendFile(__dirname +`/${jsonData[index].Image_URL}`)
+})
 
 const port = process.env.PORT || 5500;
 http.listen(port,()=>{console.log(`[server]: running on ${port}`)})
