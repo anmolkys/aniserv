7916a4b0471108e68e661345f8355a194ad5392f
anmolkys <feimoxin@outlook.com>
2023-10-30 12:54:47 +0530

    added more endpoints

--- server.js
+++ server.js
@@ -6,25 +6,44 @@ const jsonData = require("./csvjson.json");
 
 app.get("/random",(req,res)=>{
     let index = Math.floor(Math.random()*(jsonData.length+1));
-    console.log(index);
-    res.send({id:index,name: jsonData[index].Character,url:__dirname +`/${jsonData[index].Image_URL}`})
+    res.send({id:index,name: jsonData[index].Character,url:`https://waifuserv.onrender.com/waifu/${index}`})
 })
+
 app.get("/:id",(req,res)=>{
+    let index = req.params.id;
+    res.send({id:index,name: jsonData[index].Character,url:`https://waifuserv.onrender.com/waifu/${index}`})
+})
+
+
+app.get("/end/:id",(req,res)=>{
+    let id = req.params.id
+    if(id<=jsonData.length){
     function randomImage(){
         let index = Math.floor(Math.random()*5156);
         return index
     }
-    let id = req.params.id
     let list = [];
     for(let i = 0; i<id ; i++){
         let index = randomImage();
-        list = [...list,{id:index,name: jsonData[index].Character, url:`https://waifuserv.onrender.com/waifu/${index}`}]
+        list = [...list,{id:index,name: jsonData[index].Character || "unnamed" , url:`https://waifuserv.onrender.com/waifu/${index}`}]
     }
     res.send(list);
+    }
+    else{
+        res.status(404);
+        res.send({id:id,name: `File limit exceeded`, url:`File limit exceeded`})
+    }
 })
 app.get("/waifu/:index",(req,res)=>{
     let index = req.params.index;
-    res.sendFile(__dirname +`/${jsonData[index].Image_URL}`)
+    if(jsonData[index]){
+        res.sendFile(__dirname +`/${jsonData[index].Image_URL}`)
+    }
+    else{
+        res.status(404);
+        res.send({id:index,name: `File on ${index} does not exist`, url:`File on ${index} does not exist`})
+    }
+    
 })
 
 const port = process.env.PORT || 5500;
